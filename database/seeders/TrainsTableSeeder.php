7b9ec119710f7d8bfd7335fc4d576df8e94fa4d4
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-30 17:44:59 +0100

    add loop to generate trains

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Generator as Faker;
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+
+
+        for ($i = 0; $i < 20; $i++) {
+
+            $train = new Train();
+            $train->company = $faker->randomElement('Trenitalia', 'Trenord', 'Italo', 'TGV');
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->time('H_i');
+            $train->arrival_time = $faker->time('H_i');
+            $train->date = $faker->date('D_m_y');
+            $train->train_code = $faker->bothify('??-######');
+            $train->number_of_coaches = $faker->numberBetween(3, 20);
+            $train->is_delayed = $faker->boolean();
+            $train->is_delete = $faker->boolean();
+        }
+    }
+}
